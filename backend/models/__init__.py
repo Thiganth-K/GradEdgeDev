87a002fec8a6e8ecd30e91613aa234e27b76edbf
Rajakumaran <rajakumaran.offical@gmail.com>
2026-01-05 17:11:24 +0530

    Production

--- backend/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from .student import Student
-from .faculty import Faculty
-from .recruiter import Recruiter
-from .institutional import Institutional
-
-__all__ = ["Student", "Faculty", "Recruiter", "Institutional"]
